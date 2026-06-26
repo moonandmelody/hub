21ca9714ed6d99e75e0100cf8b4fe979f84a1bf8
moonandmelody <hellomoonandmelody@gmail.com>
2026-06-27 01:57:19 +0530

    Update styles.py

--- styles.py
+++ styles.py
@@ -58,6 +58,8 @@ def apply_custom_css():
                 box-shadow: 0px 4px 10px rgba(0,0,0,1);
                 text-align: center;
                 border: 1px solid {BACKGROUND_COLOR};
+                width: 210px;
+                height: 150px;
             }}
             [data-testid="stMetricLabel"] {{
                 color: white;
