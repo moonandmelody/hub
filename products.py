8f53f37b634087a46269bc034b1d840a6e8a721b
moonandmelody <hellomoonandmelody@gmail.com>
2026-06-26 13:21:48 +0530

    Update products.py

--- products.py
+++ products.py
@@ -1,8 +1,24 @@
-# FORMAT: "Item Name": Price
 # You can add as many items as you want here!
+# FORMAT: "Category Name": { "Item Name": Price, ... }
 
 CATALOG = {
-    "Midnight Luxe": 329,
-    "Moon Dance": 349,
-    "Midnight Luxe Vegan": 359
+    "Sipping Chocolate": {
+        "Midnight Luxe": 329,
+        "Moon Dance": 359,
+        "Midnight Luxe Vegan": 369
+    },
+    "Gourmet Teas": {
+        "Lemongrass Tea": 240,
+        "Chickoo Tea": 250,
+        "Mango Tea": 280
+    },
+    "Sides": {
+        "Potato Pops": 220,
+        "Mini Croissants (2)": 200,
+        "Garlic Breadsticks": 280
+    },
+    "Add Ons": {
+        "Whipped Cream": 40,
+        "Roasted Nuts": 50
+    }
 }
