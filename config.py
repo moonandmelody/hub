f8fb3fd48f40dbe7b180a152598350e607d25579
moonandmelody <hellomoonandmelody@gmail.com>
2026-06-26 12:37:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# --- SYSTEM CONFIGURATION ---
+
+# 1. Your Google Spreadsheet ID
+# (This is the long code inside your spreadsheet URL)
+SHEET_ID = "1CZwgF9I47zE7EZ_091ngwSNi2hqGc-fZnwgSY6FFjeI"
+
+# 2. Your Google Web App URL
+# (The link you copied from Apps Script > Deploy > Web App)
+MACRO_URL = "PASTE_YOUR_WEB_APP_URL_HERE" 
+
