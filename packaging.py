229045c1f2305d6f2e1085b0f0c33f4a58effdff
moonandmelody <hellomoonandmelody@gmail.com>
2026-06-29 20:55:50 +0530

    Create packaging.py

---
+++ packaging.py
@@ -0,0 +1,15 @@
+# You can add as many items as you want here!
+# FORMAT: "Category Name": { "Item Name": Price, ... }
+
+PACKAGING = {  
+  "Big Carton": 23.5,
+  "Small Carton": 20.5,
+  "Long Box with Window": 15.5,
+  "Square Box": 15.5,
+  "Big Dip Cup": 3,
+  "Small Dip Cup": 2.5,
+  "Sticker": 1.2,
+  "Ripple Cup": 3,
+  "Carry Bag": 2,
+  "Misc": 6
+}
