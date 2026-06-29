1f4abbc2754c6c87f5cdb36e1654411fd37cee12
moonandmelody <hellomoonandmelody@gmail.com>
2026-06-29 21:10:38 +0530

    Dynamic packaging.py

--- packaging.py
+++ packaging.py
@@ -1,15 +1,30 @@
-# You can add as many items as you want here!
-# FORMAT: "Category Name": { "Item Name": Price, ... }
-
-PACKAGING = {  
-  "Big Carton": 23.5,
-  "Small Carton": 20.5,
-  "Long Box with Window": 15.5,
-  "Square Box": 15.5,
-  "Big Dip Cup": 3,
-  "Small Dip Cup": 2.5,
-  "Sticker": 1.2,
-  "Ripple Cup": 3,
-  "Carry Bag": 2,
-  "Misc": 6
+import products
+# Fixed box configurations for liquid drinks
+SMALL_CARTON_COST = 20.5
+BIG_CARTON_COST = 23.5
+LONG_BOX_WITH_WINDOW = 15.5
+SQUARE_BOX = 15.5
+BIG_DIP_CUP = 3
+SMALL_DIP_CUP = 2.5
+STICKER = 1.5
+RIPPLE_CUP = 3
+CARRY_BAG = 2
+MISC = 6
+  
+# Direct item-to-profile mapping
+PACKAGING_RULES = {
+  # Liquid Drinks
+  "Midnight Luxe": {
+    "type": "liquid"
+  },
+  "Moon Dance": {
+    "type": "liquid"
+  },
+  "Midnight Luxe Vegan": {
+    "type": "liquid"
+  },
+    
+  # Food Items (Maps flat individual packaging rates)
+  "Potato Pops": {"type": "food", "pack_cost": 10.0},
+  "Classic Burger": {"type": "food", "pack_cost": 15.0}
 }
