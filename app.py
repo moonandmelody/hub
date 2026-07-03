04f2c6ca05aae8a481dbe581f9c4a1e8606d05d7
moonandmelody <hellomoonandmelody@gmail.com>
2026-07-03 13:03:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -515,12 +515,25 @@ def show_confirmation_dialog(customerName, customerContact, cart_items, total_co
     packaging_total, packaging_breakdown = calculate_order_packaging(cart_items)
     order_details, delivery_details = st.columns(2)
     with order_details:
-        order_details.write(f"Name: {customerName}")
-        order_details.write(f"Mobile: {customerContact}")
+        st.markdown(
+            f"""
+            <div id="detailsDiv" class="">
+                <h3>Order No.: #{order_id}</h3>
+                <p><b>Name:</b> {customerName}</p>
+                <p><b>Mobile:</b> {customerContact}</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        
+        #order_details.write(f"Name: {customerName}")
+        #order_details.write(f"Mobile: {customerContact}")
 
     with delivery_details:
         delivery_details.write(f"Date: {delivery_date}")
         delivery_details.write(f"Time: {delivery_time}")
+
+    st.divider()
 
     items, packaging = st.columns(2)
    
