72da0c2ddec56fa272e66fca70dc0042643172ae
moonandmelody <hellomoonandmelody@gmail.com>
2026-07-08 12:29:15 +0530

    Form entry for inventory.py

--- inventory.py
+++ inventory.py
@@ -1,8 +1,17 @@
 # inventory.py
 # Map your products to the exact entry IDs copied from the browser console
 PRODUCTS_MAP = {
-    "Apple": "entry.104857112",
-    "Banana": "entry.198421045",
-    "Orange": "entry.348102948",
-    "Mango": "entry.849201943"
+    "Midnight Luxe": "entry.433165258",
+    "Moon Dance": "entry.582659917",
+    "Midnight Luxe Vegan": "entry.861947032",
+    "Biscoff Latte": "entry.256260859",
+    "Sitaphal Coffee": "entry.91133040",
+    "Rose Tea": "entry.1500746440",
+    "Lemongrass Tea": "entry.729954979",
+    "Mini Croissants (2)": "entry.815948760",
+    "Garlic Bread": "entry.1487175716",
+    "Bun Maska": "entry.1458278148",
+    "Potato Pops": "entry.1107426365",
+    "Whipped Cream": "entry.1904146326",
+    "Roasted Nuts": "entry.1420863543"
 }
