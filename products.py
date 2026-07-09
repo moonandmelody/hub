ceda652451c083404c04fb3cb3528a905e98ac99
moonandmelody <hellomoonandmelody@gmail.com>
2026-07-09 14:27:57 +0530

    Update products.py

--- products.py
+++ products.py
@@ -9,18 +9,18 @@ CATALOG = {
         "Midnight Luxe Vegan": 369
     },
     "Gourmet Tea": {
-        "Lemongrass Tea": 240,
-        "Rose Tea": 240
+        "Rose Tea": 240,
+        "Lemongrass Tea": 240
     },
     "Gourmet Coffee": {
         "Biscoff Latte": 240,
         "Sitaphal Coffee": 250 
     },
     "Sides": {
-        "Potato Pops": 220,
         "Mini Croissants (2)": 200,
-        "Garlic Breadsticks": 280,
-        "Bun Maska": 130
+        "Bun Maska": 130,
+        "Potato Pops": 220,
+        "Garlic Breadsticks": 280
     },
     "Add Ons": {
         "Whipped Cream": 40,
