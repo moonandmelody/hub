e88fe1fff41fd25bd7918877de58e3f1c118e86d
moonandmelody <hellomoonandmelody@gmail.com>
2026-07-09 11:57:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -928,7 +928,7 @@ with st.sidebar:
             else: show_confirmation_dialog(st.session_state["form_customer"],st.session_state["form_contact"],current_cart, running_total, st.session_state.form_date, st.session_state.form_time_slot, "create")
 
 # --- 6. MAIN DASHBOARD ---
-col1, sol2 = st.columns(1,1)
+col1, sol2 = st.columns(1)
 
 with col1:
     with st.container(key="my_update_inventory_button"):
