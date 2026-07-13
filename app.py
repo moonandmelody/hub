619406ee26fada4edcd10b50aa37921b664c7464
moonandmelody <hellomoonandmelody@gmail.com>
2026-07-13 13:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1110,7 +1110,7 @@ with tab_queue:
                 current_order_items = {}
                 for col_item in df_filtered_preorders.columns:
                     if col_item not in ["Order ID", "Date", "Time", "Packaging Items", "Cost", "Packaging Cost", "Status", "Delivery Date", "Delivery Time", "Type of Order", "Customer Name", "Customer Contact", "Special Notes/Instructions", "Previous Date", "Previous Time", "Previous Items", "Previous Notes/Instructions"] and pd.notna(row[col_item]):
-                        qty = order_row[col_item]
+                        qty = row[col_item]
                         if qty != 0 and str(qty).strip() != "":
                             current_order_items[col_item] = int(float(qty))
                             st.markdown(f"• <b>{col_item}:</b> {int(float(qty))}", unsafe_allow_html=True)
